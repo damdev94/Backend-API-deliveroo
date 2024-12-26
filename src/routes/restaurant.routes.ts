1f1c0e52843d30781d69a59b19e387cf89a534ed
Damien <damien@Host-001.lan>
2024-12-26 15:56:59 +0100

    getBrandIdOk

--- src/routes/restaurant.routes.ts
+++ src/routes/restaurant.routes.ts
@@ -3,7 +3,7 @@ import RestaurantService from "../services/restaurant.service";
 
 const router = Router();
 
-const ACCESS_TOKEN = "eyJraWQiOiJ2blJoTXExN3o3K05yVVBlOUljRUFLMXI5UmRcL2xcL1ZtbVV5Umo3bVQxOVE9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJtOG9maWRlOWNrbXJvdHZxZXMzNmM5dGdkIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJodHRwczpcL1wvYXBpLWRwLmRlbGl2ZXJvby5uZXRcL2FwaV9hY2Nlc3MiLCJhdXRoX3RpbWUiOjE3MzUyMjA3MTksImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS13ZXN0LTEuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0xX09ERTdRNHBwaSIsImV4cCI6MTczNTIyMTAxOSwiaWF0IjoxNzM1MjIwNzE5LCJ2ZXJzaW9uIjoyLCJqdGkiOiJjYzQ4NWFiMi1iYTg3LTRjYzAtOGIwMC0zZTZlNDdmNmFmZTUiLCJjbGllbnRfaWQiOiJtOG9maWRlOWNrbXJvdHZxZXMzNmM5dGdkIn0.MYckreY7ZEA1-kLg-eg6ZFP9qGcAyfTEBB_E7h7-jDN82Y_siA00K4VY4kPzxvUjXt4SjTzwshhxb4i37Y-expP_vwBWClqLZmN7dscjkNi-6UB28KdmySpqwgBy0E8zFrP8Rs1xa9UvfYExBntE7W35bfPzFXr7QOJY-fjrckpz9cXSefye-2J0ss4nJCsbQgqSyOwLXY1KdV9v9A78Ze1LB5tVCmrAx7CFj_fmdmyML8ohXGS7GjihiNO8LQrNOH1prBC7xZOvd_qplmkSY8CPOhRoWriLzB8-atilmviHaAmEvoKv46FzNJkWzYCD4n3GutOJDWMpiRGZ55LsEg";
+const ACCESS_TOKEN = "eyJraWQiOiJ2blJoTXExN3o3K05yVVBlOUljRUFLMXI5UmRcL2xcL1ZtbVV5Umo3bVQxOVE9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJtOG9maWRlOWNrbXJvdHZxZXMzNmM5dGdkIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJodHRwczpcL1wvYXBpLWRwLmRlbGl2ZXJvby5uZXRcL2FwaV9hY2Nlc3MiLCJhdXRoX3RpbWUiOjE3MzUyMjI2NjYsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS13ZXN0LTEuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0xX09ERTdRNHBwaSIsImV4cCI6MTczNTIyMjk2NiwiaWF0IjoxNzM1MjIyNjY2LCJ2ZXJzaW9uIjoyLCJqdGkiOiJiMDExODE4YS04ZTA5LTQzMzQtOTBjNS01NWQ1YWNiMmYzMGIiLCJjbGllbnRfaWQiOiJtOG9maWRlOWNrbXJvdHZxZXMzNmM5dGdkIn0.C2dYrqqy4Q3HMhBx7x5tSKyeQ7JX8SqksZSKYfUB2RM6Hk90VjktTuGsRTPHhXxGKYO3MB4xUxVGCbhjN2xgrJI7jSeHqYn6aZG5C8vmqJaTrmgm1sdeh-3lXv6bnAXjlY55XJsmk55pGyKygxcGUTIixOPYxAqFMnXyYXA_vWcMRf34jJWSWUqFhQaAbPQ88IRB1ozNZ2Wq7TwCDcSv2QgIa9oNtkhBRbSRnli5RX35gcXs55lxEZLfBbz8W6S1kAe4T8eGYiR1nk_IS63s8TG40MVWNcYqoX7tr4d3VUSFTttrC_2p8vnMcPeiQzK6AFdHcDciW-FrIbEZl4wc4Q";
 
 router.get("/", async (req, res) => {
     try {
@@ -13,5 +13,14 @@ router.get("/", async (req, res) => {
         res.status(500).json(err.message);
     }
 });
+
+router.put("/upload", async (req, res) => {
+    try {
+        const menu = await new RestaurantService(ACCESS_TOKEN)
+        res.status(200).json(menu)
+    }catch(error: any) {
+
+    }
+})
 
 export default router;
